b682a6c612dea4c0f4437a13775b3939d4c724a7
FaryalAjradh <ajradfariyal@gmail.com>
2021-04-03 21:29:23 +0530

    Update Last seen timestamp and Last message in chat header and sidebar chat with dynamic values

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -53,7 +53,8 @@ function Chat() {
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className="chat__headerInfo">
                     <h3>{roomName}</h3>
-                    <p>Last Seen...</p>
+                    <p>Last Seen{" "}
+                    {new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()}</p>
                 </div>
                 <div className="chat__headerRight">
                     <IconButton>
--- client/src/components/chat/SidebarChat.js
+++ client/src/components/chat/SidebarChat.js
@@ -6,6 +6,15 @@ import {Link} from "react-router-dom";
 
 function SidebarChat({id, name, addNewChat}) {
     const [seed, setSeed] = useState("");
+    const [messages, setMessages] = useState("");
+
+    useEffect(() => {
+        if(id) {
+            db.collection('rooms').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map((doc) => doc.data())))
+            );
+        }
+    }, [id])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));        
@@ -27,7 +36,7 @@ function SidebarChat({id, name, addNewChat}) {
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className="sidebarChat__info">
                     <h2>{name}</h2>
-                    <p>This is the last message</p>
+                    <p>{messages[0]?.message}</p>
                 </div>
             </div>
         </Link>
