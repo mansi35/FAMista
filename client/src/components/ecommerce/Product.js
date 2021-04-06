2a58b07700dccf0ff18729f4287c1218f581e58b
Aditi Tiwari <tiwariaadi.18@gmail.com>
2021-04-06 15:32:40 +0530

    accepting incoming change

--- client/src/components/ecommerce/Product.js
+++ client/src/components/ecommerce/Product.js
@@ -126,6 +126,11 @@ function Product({id, title, image, price, rating, quantity, userId, setLength})
                 <p>Modal</p>
             </ShareProductModal>
             <div>
+            <center  className="product__options">
+            <button onClick={seeTwinCount}>Twin Count</button>
+            <button onClick={addToBasket}>Add to Basket</button>
+            <button onClick={showModal}>Share Product</button>
+            </center>
             </div>
         </div>
     )
